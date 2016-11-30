79ce7f0d99b8aba179fe215fd62ea1b12ac190d4
JakubBalcerowicz <ku44@wp.pl>
2016-11-30 11:40:16 +0100

    Create przyklad.c

---
+++ algorytmy/przyklad.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string>
+
+typedef struct Dane{
+
+    char word[255];
+    }D;
+
+int main{
+
+
+printf("%s slowo struktury",Dane);
+
+return 0;
+}
+
+http://4programmers.net/C/Struktury
